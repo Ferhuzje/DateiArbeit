55af794f6ed12f07fa362e525d54cb7c8471d3f1
Ferhuzje <86949390+Ferhuzje@users.noreply.github.com>
2021-09-07 12:25:33 +0200

    Artikel implements Serializable

---
+++ src/comcave/Artikel.java
@@ -0,0 +1,35 @@
+package comcave;
+
+import java.io.Serializable;
+
+public class Artikel implements Serializable {
+	
+	private String bezeichnung;
+	private double preis;
+	
+	public Artikel(String bezeichnung, double preis) {
+		this.bezeichnung = bezeichnung;
+		this.preis = preis;
+	}
+	
+	
+	public String getBezeichnung() {
+		return bezeichnung;
+	}
+
+	public void setBezeichnung(String bezeichnung) {
+		this.bezeichnung = bezeichnung;
+	}
+
+	
+	public double getPreis() {
+		return preis;
+	}
+
+
+
+
+	public void setPreis(double preis) {
+		this.preis = preis;
+	}
+}
