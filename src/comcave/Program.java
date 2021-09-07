0213bb0ed90999f96522c8ae4ed2dd165b1670a8
Ferhuzje <86949390+Ferhuzje@users.noreply.github.com>
2021-09-07 09:11:41 +0200

    try catch mit BufferedWriter

--- src/comcave/Program.java
+++ src/comcave/Program.java
@@ -1,5 +1,12 @@
 package comcave;
 
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+
 public class Program {
 
 	public static void main(String[] args) {
@@ -30,8 +37,27 @@ public class Program {
 //		}
 //		catch(FileNotFoudException e2) {
 //		}
-	}
+
 	
+	
+		try {
+			
+			// Datenstrom erzeugen, Datei öffnen
+			FileWriter fw = new FileWriter("datei.txt"); 
+			BufferedWriter bw = new BufferedWriter(fw);
+			bw.write("Daten mit Java geschrieben");
+			bw.newLine();
+			bw.write("die zweite Zeile Daten mit Java geschrieben");
+			//Datei schließen
+			bw.close();
+			System.out.println("Daten gespeichert");
+			}
+		catch(Exception e) {
+			System.out.println("Daten konnten nicht gespeichert werden");
+		}
+			
+		}
+		
 	
 
 }
