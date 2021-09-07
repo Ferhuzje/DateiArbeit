ee773940525123cfdd474b30b68d5a0b174eb03d
Ferhuzje <86949390+Ferhuzje@users.noreply.github.com>
2021-09-07 11:44:50 +0200

    binaerDateiSchreiben

--- src/comcave/Program.java
+++ src/comcave/Program.java
@@ -3,22 +3,48 @@ package comcave;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
+import java.io.RandomAccessFile;
+
 
 public class Program {
 
 	public static void main(String[] args) {
 		textDateiSchreiben();
+		binaerDateiSchreiben();		
 	}
+	
+	
+	private static void binaerDateiSchreiben() {
+		int i = 100;
+		double d = 99.99;
+		boolean b = true;
+		String s = "Java";
+		
+		try {
+			RandomAccessFile file = new RandomAccessFile("binaerdatei.bin", "rw");
+			file.writeInt(i);
+			file.writeDouble(d);
+			file.writeBoolean(b);;
+			file.writeUTF(s);
+			file.close();
+			System.out.println("Schreibvorgang fertig");
+			} 
+		catch (IOException e) {
+			System.out.println("Datei - Fehler");
+		}
+		
+	}
+	
 	
 	private static void textDateiSchreiben() {
 		
 		try {
 			int[] arrayTest = {1,2,3};
 			arrayTest[3] = 100;
-			
 			System.out.println("try ende");
 		}
 		catch(Exception e) {
@@ -32,7 +58,9 @@ public class Program {
 //		catch(IOException e3) {
 //
 //		}
-//		catch(FileNotFoudException e2) {
+//		catch(FileNotFoudException e) {
+//		e.printStackTrace();
+			
 //		}
 
 	
@@ -47,6 +75,7 @@ public class Program {
 			bw.close();
 			System.out.println("Daten gespeichert");
 			}
+		//fängt _alle_ Fehler ab
 		catch(Exception e) {
 			System.out.println("Daten konnten nicht gespeichert werden");
 		}
